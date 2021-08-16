4cd9adf4d706e40c810d0ad47cbf5859201c6774
Thiago Henrique <thiagohenriquelemoscosta@gmail.com>
2021-08-16 14:01:38 -0300

    part02

--- TilesPunk/Assets/PlayerMovement.cs
+++ TilesPunk/Assets/PlayerMovement.cs
@@ -6,6 +6,8 @@ public class PlayerMovement : MonoBehaviour {
 
 	public CharacterController2D controller;
 
+	public Animator animator;
+
 	public float runSpeed = 40f;
 
 	float horizontalMove = 0f;
@@ -16,6 +18,8 @@ public class PlayerMovement : MonoBehaviour {
 	void Update () {
 
 		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+		animator.SetFloat ("Speed", Mathf.Abs (horizontalMove));
 
 		if (Input.GetButtonDown("Jump"))
 		{
